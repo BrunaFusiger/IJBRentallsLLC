15b982866c46db46e69fd42af4d768ea16d0e317
Bruna <bruna.fusiger@gmail.com>
2022-08-30 15:11:06 -0300

    first components

---
+++ src/components/Button.vue
@@ -0,0 +1,39 @@
+<template>
+    <router-link to="/apartaments" class="button">{{  msg  }}</router-link>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Button',
+    props: {
+        msg: String
+    },
+})
+</script>
+
+<style scoped lang="scss">
+.button {
+    background-color: $baseGreen;
+    opacity: 0.8;
+    border-radius: 20px;
+
+    padding: .5rem 1rem;
+
+    color: #fff;
+    text-decoration: none;
+    text-transform: uppercase;
+    letter-spacing: .1rem;
+    font-weight: 400;
+
+    transition: .5s;
+
+    &:hover {
+        background-color: rgba($color: #fff, $alpha: .8);
+        border: 1px solid $baseGreen;
+        color: $baseGreen;
+        transform: scale(1.05);
+    }
+}
+</style>
