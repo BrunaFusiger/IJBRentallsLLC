a44a3c08b55fe9ed4c655dc1a19b385d9e83be32
Bruna <bruna.fusiger@gmail.com>
2022-08-30 15:11:35 -0300

    new routes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <router-view/>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { defineComponent } from 'vue'
 
-nav {
-  padding: 30px;
-}
+export default defineComponent({
+})
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+<style lang="scss">
+  * {
+    background-color: aquamarine;
+    font-family: 'Open Sans';
+    letter-spacing: 0.05em;
+    font-size: 1rem;
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+    a, p {
+      text-decoration: none;
+      color: #fff;
+
+    }
+  }
 </style>
